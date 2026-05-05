fe7f70cd163fd648455fcc86c47c576ffe840fa9
user <@>
2026-05-05 09:32:37 +0300

    Create Fastapi2 Project

---
+++ main.py
@@ -0,0 +1,41 @@
+
+from fastapi import FastAPI
+from pydantic import EmailStr, BaseModel
+
+import uvicorn
+
+from items_views import router as items_router
+from users.views import router as users_router
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello index",
+    }
+
+
+@app.get("/hello")
+def hello(name: str = "Гость"):
+    name = name.strip().title()
+    return {"message": f"Привет {name}!"}
+
+
+
+@app.get("/calc/add")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "Результат:": a + b,
+    }
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
